8d8e2b49ea06f48b3e27d1ccd405a0d7c81fd311
mgerfer1112 <m.gervilla1112@gmail.com>
2025-03-03 19:54:11 +0100

    Create 5-ca.sh

---
+++ Scripts/5-ca.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+
+# Configuramos para mostrar los comandos y finalizar
+set -ex
+
+# Importamos el archivo .env
+source .env
+
+# Actualizamos el sistema
+apt update -y
+
+# Actualiza paquetes
+apt upgrade -y  
+
+#Importar la solicitud de certificado
+#sudo EASYRSA_PKI="$EASYRSA_PKI" /home/$SUDO_USER/easy-rsa/easyrsa import-req /tmp/client1.req client1
+
+#Firmar
+#sudo EASYRSA_PKI="$EASYRSA_PKI" /home/$SUDO_USER/easy-rsa/easyrsa sign-req client client1
+
+# copie los archivos server.crty ca.crtdel servidor CA al servidor OpenVPN:
+scp -i $RutaPEM2 /home/ubuntu/easy-rsa/pki/issued/client1.crt $UsuarioVPN@$IPPrivadaVPN:/tmp
